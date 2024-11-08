03bf10f045369dd88e5413b1db30a29942933ca3
Onyekelu Chukwuebuka <onyekeluchukwuebuka@yahoo.ca>
2024-11-08 01:07:40 +0100

    fixed

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 # The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard. a program that solves the N queens problem.
 
 # Usage: nqueens N
